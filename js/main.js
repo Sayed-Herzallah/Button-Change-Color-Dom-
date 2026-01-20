19b1bbb40f7ddec59d2338346add1c2ad9b5f469
Sayed Herzallah <78576810+Sayed-Herzallah@users.noreply.github.com>
2026-01-20 14:12:01 +0200

    Update main.js
    Removed commented-out code related to color change functionality.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,7 @@
 
-// getElementById تتعامل مع عنصر واحد فقط حتي لو عندي اكثر من عنصر واخد اي دي 
-// getElementsByClassName تتعامل مع عناصر متعددة حتي لو عندي اكثر من عنصر واخد اسم الكلاس ولكن لازم تنادي
-// getElementsByName تتعامل مع عنصر واحد فقط حتي لو عندي اكثر من عنصر واخد اسم الاسم ولكن لازم تنادي 
-
-// document.querySelector
-
-
-
-// document.querySelector("#red").addEventListener("click",function(){
-//     document.querySelector("#text").style.color = "red"
-// })
-// document.querySelector("#green").addEventListener("click",function(){
-//     document.querySelector("#text").style.color = "green"
-// })
-// document.querySelector("#yellow").addEventListener("click",function(){
-//     document.querySelector("#text").style.color = "yellow"
-// })
 document.querySelectorAll("button").forEach(function(item){
     item.addEventListener("click",function(){
         document.querySelector("#text").style.color = item.id
     })
+
 })
